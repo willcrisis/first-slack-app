6a679e6b6159731ca0c6d34354dac841ede67769
Willian Krause <krause.willian@gmail.com>
2018-03-01 15:10:08 -0300

    db-restore controller

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+const dbRestore = require('./routes/db-restore');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/db-restore', dbRestore);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
