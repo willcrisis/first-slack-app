82eb375fd9367d35fd5f77f8084f5cdffa2a1eab
Willian Krause <krause.willian@gmail.com>
2018-03-01 15:15:14 -0300

    db-restore controller

--- routes/db-restore.js
+++ routes/db-restore.js
@@ -1,10 +1,4 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/db-restore', function(req, res, next) {
+module.exports = (req, res, next) => {
     console.log(req);
     res.status(200).send();
-});
-
-module.exports = router;
+};
